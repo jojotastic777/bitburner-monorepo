214f6569dbee7972832e3b9f3d423479d29701a0
jojotastic777 <jojotastic777@gmail.com>
2022-01-13 15:24:03 -0800

    Implemented a whole-network replacement for the `free` command.

---
+++ packages/bitburner-scripts/src/bin/util/deepfree.ts
@@ -0,0 +1,13 @@
+import { NS } from "@global/bitburner";
+import { scan } from "/lib/scan.js";
+
+export async function main(ns: NS) {
+    const SERVERS = scan(ns).concat(ns.getPurchasedServers()).map(host => ns.getServer(host))
+    const TOTAL_RAM = SERVERS.map(srv => srv.maxRam).reduce((acc, cur) => acc + cur)
+    const USED_RAM = SERVERS.map(srv => srv.ramUsed).reduce((acc, cur) => acc + cur)
+    const FREE_RAM = TOTAL_RAM - USED_RAM
+    
+    ns.tprintf(`Total:     ${ns.nFormat(TOTAL_RAM * 1e9, "0.00b")}`)
+    ns.tprintf(`Used:      ${ns.nFormat(USED_RAM * 1e9, "0.00b")} (${ns.nFormat(USED_RAM / TOTAL_RAM * 100, "0.00")}%%)`)
+    ns.tprintf(`Available: ${ns.nFormat(FREE_RAM * 1e9, "0.00b")} (${ns.nFormat(FREE_RAM / TOTAL_RAM * 100, "0.00")}%%)`)
+}
