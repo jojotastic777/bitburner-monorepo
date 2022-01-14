b52bc26da2e225e052179c85a5556d2b2ba6665d
jojotastic777 <jojotastic777@gmail.com>
2022-01-14 00:04:31 -0800

    Remove unused utility library, replaced by ramda.

--- packages/bitburner-scripts/src/lib/utility.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Deduplicate an array by converting it to a Set and back.
- * @param array The array to be de-duplicated.
- * @returns An array lacking any duplicate elements.
- */
-export function dedupe<T>(array: T[]): T[] {
-    return [ ...new Set(array) ]
-}
