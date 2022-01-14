4c9de62fe664b96ca18518ee03f0e95734b61d97
jojotastic777 <jojotastic777@gmail.com>
2022-01-14 02:03:58 -0800

    Update hwgMan to use fs library.

--- packages/bitburner-scripts/src/bin/svc/hwgMan.ts
+++ packages/bitburner-scripts/src/bin/svc/hwgMan.ts
@@ -7,45 +7,12 @@
 
 import { NS } from "@global/bitburner";
 import { deploy } from "../../lib/deploy";
+import { readFile } from "../../lib/fs";
 
 const CONFIG_PATH = "/etc/svc/hwgMan/config.txt"
 
 type HWGManagerConfiguration = {
     hackTarget: string
-}
-
-/**
- * Removes "dist/bitburner" from the start of the filename.
- * Adds a leading "/" if one is needed.
- * Removes a leading "/" if one isn't needed.
- * 
- * @param filename The filename to be normalized.
- * @returns A normalized filename.
- */
-function normalizeFilename(filename: string): string {
-    // Remove the unneeded folder prefix.
-    filename = filename.replace(/^dist\/bitburner/, "")
-
-    // If the filename is lacking a neccesary leading "/", add one.
-    filename = filename[0] !== "/" && filename.slice(1).includes("/") ? "/" + filename : filename
-
-    // If the filename has an extra leading "/", remove it.
-    filename = filename[0] === "/" && !filename.slice(1).includes("/") ? filename.slice(1) : filename
-
-    return filename
-}
-
-/**
- * A wrapper for ns.read, which normalizes the filename and copies the requested file from "home" before reading.
- * @param ns A Netscript context.
- * @param filename The name of the file to read.
- * @returns If the file exists, returns its contents. Otherwise, returns an empty string.
- */
-async function readFile(ns: NS, filename: string): Promise<string> {
-    let normFilename = normalizeFilename(filename)
-
-    await ns.scp(normFilename, "home", ns.getHostname())
-    return await ns.read(normFilename)
 }
 
 /**
