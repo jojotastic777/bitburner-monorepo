2d4501334340a904c482ee3ce824b8ca32e01e5c
jojotastic777 <jojotastic777@gmail.com>
2022-01-14 01:34:41 -0800

    Wrote a new logger library.

---
+++ packages/bitburner-scripts/src/lib/log.ts
@@ -0,0 +1,46 @@
+import { NS } from "@global/bitburner"
+import * as R from "ramda"
+
+export type LogLevel = "none" | "debug" | "info" | "warning" | "error"
+
+export default R.curry((ns: NS, scriptName: string, logLevel: LogLevel) => ({
+    debug: (message: string): void => {
+        let logString = `[${scriptName}] [${logLevel.toUpperCase()}]: ${message}`
+
+        if ([ "debug" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    },
+
+    info: (message: string): void => {
+        let logString = `[${scriptName}] [${logLevel.toUpperCase()}]: ${message}`
+
+        if ([ "debug", "info" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    },
+
+    warning: (message: string): void => {
+        let logString = `[${scriptName}] [${logLevel.toUpperCase()}]: ${message}`
+
+        if ([ "debug", "info", "warning" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    },
+
+    error: (message: string): void => {
+        let logString = `[${scriptName}] [${logLevel.toUpperCase()}]: ${message}`
+
+        if ([ "debug", "info", "warning", "error" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    }
+}))
