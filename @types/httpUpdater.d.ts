59711dd165ed5e024deec979206d06c2530ef5bc
jojotastic777 <jojotastic777@gmail.com>
2022-01-14 16:13:41 -0800

    Wrote an HTTP-based (non-WebSocket) updater. Started a second bitburner scripts folder, for re-writing everything.

---
+++ @types/httpUpdater.d.ts
@@ -0,0 +1,3 @@
+export type Manifest = {
+    [filename: string]: string
+}
---
+++ packages/http-update-server/src/index.ts
@@ -0,0 +1,27 @@
+import fs from "fs"
+import glob from "glob"
+import express from "express"
+import { Manifest } from "@global/httpUpdater"
+
+const PORT = parseInt(process.env.PORT ?? "63845")
+
+const buildManifest = (): Manifest => {
+    const filenames = glob.sync("**/*.js")
+    const manifest: Manifest = {}
+
+    filenames.forEach(filename => {
+        manifest[filename] = fs.readFileSync(filename).toString()
+    })
+
+    return manifest
+}
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json(buildManifest()).end()
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`)
+})
