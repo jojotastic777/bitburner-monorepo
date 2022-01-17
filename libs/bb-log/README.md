48fc079dfe5192fd597be8ec093041fdaaa28b63
jojotastic777 <jojotastic777@gmail.com>
2022-01-16 23:42:01 -0800

    Copied over logger lib.

---
+++ libs/bb-log/README.md
@@ -0,0 +1,3 @@
+# @bitburner-monorepo-template/bb-log
+
+A library, potentially usable by both scripts and apps. Add some description here.
---
+++ libs/bb-log/src/index.ts
@@ -0,0 +1,46 @@
+import { NS } from "@global/bitburner"
+import * as R from "ramda"
+
+export type LogLevel = "none" | "debug" | "info" | "warning" | "error"
+
+export default R.curry((ns: NS, scriptName: string, logLevel: LogLevel) => ({
+    debug: (message: string): void => {
+        const logString = `[${scriptName}] [DEBUG]: ${message}`
+
+        if ([ "debug" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    },
+
+    info: (message: string): void => {
+        const logString = `[${scriptName}] [INFO]: ${message}`
+
+        if ([ "debug", "info" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    },
+
+    warning: (message: string): void => {
+        const logString = `[${scriptName}] [WARN]: ${message}`
+
+        if ([ "debug", "info", "warning" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    },
+
+    error: (message: string): void => {
+        const logString = `[${scriptName}] [ERROR]: ${message}`
+
+        if ([ "debug", "info", "warning", "error" ].includes(logLevel)) {
+            ns.tprintf(logString)
+        }
+
+        ns.print(logString)
+    }
+}))
